7c216b3f143b71d42c05e41baf3f6c3a61e8fe14
Jonas Svarvaa <jonassvarvaa@gmail.com>
2011-11-08 12:20:51 +0100

    Solution for P4 added.

---
+++ ap4.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+
+def ispalindrome(word):
+    return word == word[::-1]
+
+min = 100
+max = 1000 # (999)
+
+palin = []
+
+for x in range(min, max, 1):
+    for y in range(min, max, 1):
+        if ispalindrome(str(x*y)):
+            palin.append(x*y)
+
+palin.sort()
+print(palin)
