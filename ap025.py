7970de44d8ad36417ec55eb3d6316f99c37d0d1e
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-01-17 10:25:53 +0100

    Solved problem 25.

---
+++ ap025.py
@@ -0,0 +1,12 @@
+def depth(n):
+	return len(str(n))
+
+def fib(n):
+	return (n[len(n)-2]+n[len(n)-1])
+
+n = [1, 1]
+digits = 1000
+while(depth(n[len(n)-1])) < 1000:
+	n.append(fib(n))
+
+print("f(%s) = " % len(n), n[len(n)-1])
