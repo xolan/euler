4f61841b54a1f6bc7166dc194095253ff9c98747
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-01-17 07:27:27 +0100

    Modified solution 21. No longer requires clint, and is more modular.

--- ap021.py
+++ ap021.py
@@ -1,5 +1,3 @@
-from clint.textui import progress
-
 def d(n):
 	cnt = 0
 	for i in range(1, n, 1):
@@ -7,9 +5,12 @@ def d(n):
 			cnt += int(i)
 	return cnt
 
-done = []
-for n in progress.bar(range(1, 10000, 1), label="Evaluating "):
-	if(n == d(d(n)) and n != d(n)):
-		done.append(n)
+def am(max):
+	s = 0
+	for n in range(1, max, 1):
+		if(n == d(d(n)) and n != d(n)):
+			s += n
+	return s
 
-print("Sum is: %s" % sum(done))
+if __name__ == '__main__':
+	print("Sum is: %s" % am(10000))
